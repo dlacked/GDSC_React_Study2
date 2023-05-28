39ac4958eb7f1e10b59ca7ebcf8eb8d265c2fe6f
Changyong IM <imcy0104@gmail.com>
2023-05-28 22:05:29 +0900

    63. 7-11. 최적화

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,3 +1,4 @@
+import React from "react";
 import MyButton from "./MyButton";
 import { useNavigate  } from "react-router-dom";
 
@@ -43,4 +44,4 @@ const DiaryItem = ({id, emotion, content, date}) => {
     )
 }
 
-export default DiaryItem;
+export default React.memo(DiaryItem);
